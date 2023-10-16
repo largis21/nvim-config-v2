83c4ec37c41b44e3c85b78f202bef574b7caeec5
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2023-10-16 08:07:27 +0200

    commenting jsx

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,5 +10,15 @@ return {
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
+  dependencies = {
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      config = function()
+        require("Comment").setup({
+          pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+        })
+      end
+    }
+  }
 }
